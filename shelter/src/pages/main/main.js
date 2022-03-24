c90b23a656b477861321d2c05ba476a896be2c25
eprikhodko <eprikhodkodev@gmail.com>
2022-03-24 22:57:47 +0300

    init: 'npm install --save-dev html-webpack-plugin'

---
+++ shelter/src/pages/main/main.js
@@ -0,0 +1,3 @@
+// import './scss/style.scss';
+
+console.log('hello, SASS, this is main page');
--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -1,9 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/pages/main/index.html',
+    }),
+  ],
+  entry: './src/pages/main/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     // __dirname means current directory, where webpack.config.js is located
     path: path.resolve(__dirname, 'dist'),
   },
