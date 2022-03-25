6ff9904f16fdac71d31de7faa2e4b487e1f287c3
eprikhodko <eprikhodkodev@gmail.com>
2022-03-25 12:47:36 +0300

    init: add html images support via html-loader

--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
         test: /\.(sa|sc|c)ss$/,
         use: [
           // Extract styles into a separate file
