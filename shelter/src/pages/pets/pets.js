832914e4b7d1188a4a6b8d9cd761eda1f938811a
eprikhodko <eprikhodkodev@gmail.com>
2022-03-25 10:59:45 +0300

    init: add multipage html setup to webpack

--- shelter/src/pages/pets/pets.js
+++ shelter/src/pages/pets/pets.js
@@ -1,3 +1,3 @@
 // import './scss/style.scss';
 
-console.log('hello, SASS');
+console.log('hello, this is pets page');
--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -5,16 +5,30 @@ module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
+      inject: true,
       template: './src/pages/main/main.html',
+      // devserver dont work with filename property
+      filename: 'main.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/pages/pets/pets.html',
+      filename: 'pets.html',
+      chunks: ['pets'],
     }),
   ],
-  entry: './src/pages/main/main.js',
+  // entry: './src/pages/main/main.js',
+  entry: {
+    main: './src/pages/main/main.js',
+    pets: './src/pages/pets/pets.js',
+  },
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    filename: '[name].js',
     // __dirname means current directory, where webpack.config.js is located
     path: path.resolve(__dirname, 'dist'),
   },
-
   devServer: {
     watchFiles: ['src/**/*'],
   },
