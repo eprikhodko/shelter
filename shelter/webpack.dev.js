092c98a6387aa91dc9776d55e4ed0abedff9a7f4
eprikhodko <eprikhodkodev@gmail.com>
2022-03-26 11:59:03 +0300

    init: update webpack.dev.js

--- shelter/webpack.dev.js
+++ shelter/webpack.dev.js
@@ -5,14 +5,6 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
   mode: 'development',
 
-  output: {
-    filename: '[name].js',
-    // __dirname means current directory, where webpack.config.js is located
-    path: path.resolve(__dirname, 'dist'),
-    // put images into /dist/assets folder
-    assetModuleFilename: 'assets/[hash][ext][query]',
-  },
-
   devtool: 'source-map',
 
   devServer: {
