eb6771cafb3e8734638d53c8a4d1cafc1d87bf66
eprikhodko <eprikhodkodev@gmail.com>
2022-04-14 20:49:34 +0300

    feat: add working mobile menu for main page

--- shelter/src/pages/main/main.js
+++ shelter/src/pages/main/main.js
@@ -1,3 +1,15 @@
 import '../../scss/main.scss';
 
 console.log('hello, this is main page');
+
+const closeButton = document.querySelector('.main-header__button-close-nav');
+const openButton = document.querySelector('.main-header__button-open-nav');
+const nav = document.querySelector('.main-header__nav-list-wrapper');
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('navigation-open');
+});
+
+openButton.addEventListener('click', () => {
+  nav.classList.add('navigation-open');
+});
