c0ad04f53a8e8591a6cc6e12e95ac4160ac2f14f
eprikhodko <eprikhodkodev@gmail.com>
2022-04-14 21:41:45 +0300

    feat: disable scroll when mobile menu is open and hide mobile menu when user clicks any link

--- shelter/src/pages/main/main.js
+++ shelter/src/pages/main/main.js
@@ -5,11 +5,25 @@ console.log('hello, this is main page');
 const closeButton = document.querySelector('.main-header__button-close-nav');
 const openButton = document.querySelector('.main-header__button-open-nav');
 const nav = document.querySelector('.main-header__nav-list-wrapper');
+const navItems = document.querySelectorAll('.main-header__nav-list-item');
+const body = document.querySelector('body');
 
 closeButton.addEventListener('click', () => {
   nav.classList.remove('navigation-open');
+  body.classList.remove('disable-scroll');
 });
 
 openButton.addEventListener('click', () => {
   nav.classList.add('navigation-open');
+  body.classList.add('disable-scroll');
 });
+
+// close mobile nav when user click any item from mobile menu
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('navigation-open');
+    body.classList.remove('disable-scroll');
+  });
+});
+
+console.log(body);
