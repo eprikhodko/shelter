f873176903316e0ffa7eaac8e8ffd8148a0adfeb
eprikhodko <eprikhodkodev@gmail.com>
2022-04-17 13:38:53 +0300

    feat: upload new deploy for shelter-adaptive task

---
+++ shelter/pets.637c262249ad82735cf9.js
@@ -0,0 +1,38 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+console.log('hello, this is pets page');
+
+const closeButton = document.querySelector('.header-nav__button-close-nav');
+const openButton = document.querySelector('.header-nav__button-open-nav');
+const nav = document.querySelector('.header-nav__nav-list-wrapper');
+const navItems = document.querySelectorAll('.header-nav__nav-list-item');
+const body = document.querySelector('body');
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('navigation-open');
+  body.classList.remove('disable-scroll');
+});
+
+openButton.addEventListener('click', () => {
+  nav.classList.add('navigation-open');
+  body.classList.add('disable-scroll');
+});
+
+// close mobile nav when user click any item from mobile menu
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    nav.classList.remove('navigation-open');
+    body.classList.remove('disable-scroll');
+  });
+});
+
+console.log(closeButton);
+console.log(openButton);
+console.log(nav);
+
+/******/ })()
+;
+//# sourceMappingURL=pets.637c262249ad82735cf9.js.map
