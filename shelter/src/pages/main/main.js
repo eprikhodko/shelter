1e292d3e890565cdb440e9a2cadf6ca10d5e108b
eprikhodko <eprikhodkodev@gmail.com>
2022-04-21 11:03:32 +0300

    feat: generate new elements for left item

--- shelter/src/pages/main/main.js
+++ shelter/src/pages/main/main.js
@@ -35,6 +35,8 @@ fetch('../../assets/js/pets.json')
 const BTN_LEFT = document.querySelector('#btn-left');
 const BTN_RIGHT = document.querySelector('#btn-right');
 const CAROUSEL = document.querySelector('#carousel');
+const ITEM_LEFT = document.querySelector('#item-left');
+const ITEM_RIGHT = document.querySelector('#item-right');
 
 const moveLeft = () => {
   CAROUSEL.classList.add('transition-left');
@@ -57,6 +59,32 @@ CAROUSEL.addEventListener('animationend', (animationEvent) => {
 
     const leftItems = document.querySelector('#item-left').innerHTML;
     document.querySelector('#item-active').innerHTML = leftItems;
+
+    // создадим новый элемент
+    const card1 = document.createElement('div');
+    card1.classList.add('card');
+    // запишем внутрь этого элемента рандомное число
+    card1.innerText = Math.floor(Math.random() * 8);
+
+    // создадим новый элемент
+    const card2 = document.createElement('div');
+    card2.classList.add('card');
+    // запишем внутрь этого элемента рандомное число
+    card2.innerText = Math.floor(Math.random() * 8);
+
+    // создадим новый элемент
+    const card3 = document.createElement('div');
+    card3.classList.add('card');
+    // запишем внутрь этого элемента рандомное число
+    card3.innerText = Math.floor(Math.random() * 8);
+
+    // удалим все что было внутри левого айтема
+    ITEM_LEFT.innerHTML = '';
+    // добавим новые элементы внутрь левого айтема
+    // можно заменить это на цикл
+    ITEM_LEFT.appendChild(card1);
+    ITEM_LEFT.appendChild(card2);
+    ITEM_LEFT.appendChild(card3);
   } else {
     CAROUSEL.classList.remove('transition-right');
   }
