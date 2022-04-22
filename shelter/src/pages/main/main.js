5b6f0cdc342ca6bfeab31a0ea68d03b2f31f59a5
eprikhodko <eprikhodkodev@gmail.com>
2022-04-22 10:57:48 +0300

    feat: create working js code for desktop carousel

--- shelter/src/pages/main/main.js
+++ shelter/src/pages/main/main.js
@@ -28,9 +28,6 @@ navItems.forEach((item) => {
 });
 
 // code for carousel
-// fetch('../../assets/js/pets.json')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
 
 const url = '../../assets/js/pets.json';
 async function getData(url) {
@@ -50,40 +47,10 @@ const ITEM_LEFT = document.querySelector('#item-left');
 const ITEM_RIGHT = document.querySelector('#item-right');
 const ITEM_ACTIVE = document.querySelector('#item-active');
 
-// const generateUniquePetsKeys = () => {
-//   function shuffle(array) {
-//     let currentIndex = array.length,
-//       randomIndex;
-
-//     // While there remain elements to shuffle.
-//     while (currentIndex != 0) {
-//       // Pick a remaining element.
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex--;
-
-//       // And swap it with the current element.
-//       [array[currentIndex], array[randomIndex]] = [
-//         array[randomIndex],
-//         array[currentIndex],
-//       ];
-//     }
-
-//     return array;
-//   }
-
-//   // Used like so
-//   let arr = [0, 1, 2, 3, 4, 5, 6, 7];
-//   shuffle(arr);
-//   return shuffle(arr);
-// };
-
-// let uniqueNumbers = generateUniquePetsKeys();
-let uniqueNumbers = [4, 0, 2];
-console.log(uniqueNumbers);
+// set initial pets indexes for carousel
+let petsNums = [4, 0, 2];
 
 const generateNewNumbersBasedOnOldNumbers = (oldNums) => {
-  // console.log('this is old nums', oldNums);
-  // console.log('this is first three numbers', oldNums.slice(0, 3));
   function shuffle(array) {
     let currentIndex = array.length,
       randomIndex;
@@ -104,20 +71,15 @@ const generateNewNumbersBasedOnOldNumbers = (oldNums) => {
     return array;
   }
 
-  // Used like so
   let arr = [0, 1, 2, 3, 4, 5, 6, 7];
-  // shuffle(arr);
 
   let shuffledArray = shuffle(arr);
-  // console.log('this is shuffled array', shuffledArray.slice(0, 3));
 
-  // нужно что? Смержить старый массив из трех чисел, и новый шаффл массив. Потом сделать из этого массива сет. Если длина сета такая же как длина смерженного массива, тогда дубликатов нет, и мы возвращаем shuffledArray. Если же дубликаты есть, то нам нужно запустить эту функцию снова. Base case - когда у нас сет равен длине массива.
+  // смержим старый массив из трех чисел, и новый шаффл массив. Потом сделаем из этого массива сет. Если длина сета такая же как длина смерженного массива, тогда дубликатов нет, и мы возвращаем shuffledArray. Если же дубликаты есть, то нам нужно запустить эту функцию снова. Base case - когда у нас сет равен длине массива.
   const mergedArray = shuffledArray.slice(0, 3).concat(oldNums.slice(0, 3));
-  // console.log([...new Set(mergedArray)]);
-  // console.log(mergedArray.length === [...new Set(mergedArray)].length);
 
   if (mergedArray.length === [...new Set(mergedArray)].length) {
-    uniqueNumbers = shuffledArray.slice(0, 3);
+    petsNums = shuffledArray.slice(0, 3);
     return shuffledArray.slice(0, 3);
   } else {
     return generateNewNumbersBasedOnOldNumbers(oldNums);
@@ -130,77 +92,34 @@ const moveLeft = () => {
   BTN_RIGHT.removeEventListener('click', moveRight);
 
   ITEM_LEFT.innerHTML = '';
-  generateNewNumbersBasedOnOldNumbers(uniqueNumbers);
+  generateNewNumbersBasedOnOldNumbers(petsNums);
 
   // сгенерируем три карточки для левого айтема
   for (let i = 0; i < 3; i += 1) {
-    setLeftItem(uniqueNumbers[i]);
+    setLeftItem(petsNums[i]);
   }
 
-  const currentNumbers = uniqueNumbers;
-  console.log('current numbers state', currentNumbers);
-  // generateNewNumbersBasedOnOldNumbers(uniqueNumbers);
-  // console.log('updated numbers state', uniqueNumbers);
+  console.log('current numbers state', petsNums);
 };
 
 const moveRight = () => {
   CAROUSEL.classList.add('transition-right');
   BTN_RIGHT.removeEventListener('click', moveRight);
   BTN_LEFT.removeEventListener('click', moveLeft);
+
+  ITEM_RIGHT.innerHTML = '';
+  generateNewNumbersBasedOnOldNumbers(petsNums);
+
+  // сгенерируем три карточки для левого айтема
+  for (let i = 0; i < 3; i += 1) {
+    setRightItem(petsNums[i]);
+  }
 };
 
 BTN_LEFT.addEventListener('click', moveLeft);
 BTN_RIGHT.addEventListener('click', moveRight);
 
-const setInitialCentralItem = (petKey) => {
-  // ITEM_ACTIVE.innerHTML = '';
-
-  const createCardTemplate = () => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    return card;
-  };
-
-  // создадим карточку питомца
-  // внутри дива мы можем создавать разные элементы, например createElementImage, createElementButton, итд, и все это положить внутрь нашего дива при помощи appendChild, нужно не забыть задать новым элементам новые классы.
-  const card1 = createCardTemplate();
-
-  // const petKey = Math.floor(Math.random() * 7);
-  // создадим элемент img
-  const img = document.createElement('img');
-  img.classList.add('card__image');
-  // зададим элементу img src
-  img.src = petsData[petKey].img;
-  // добавим элемент img внутрь нашей карточки
-  card1.appendChild(img);
-
-  // добавим в карточку имя питомца
-  const petName = document.createElement('p');
-  petName.classList.add('card__pet-name');
-  // console.log(petName);
-  petName.innerText = petsData[petKey].name;
-  card1.appendChild(petName);
-
-  // добавим в карточку кнопку learn more
-  const buttonLearnMore = document.createElement('button');
-  buttonLearnMore.type = 'button';
-  buttonLearnMore.classList.add('button-secondary');
-  buttonLearnMore.classList.add('card__button');
-  buttonLearnMore.innerText = 'Learn more';
-  card1.appendChild(buttonLearnMore);
-
-  // добавим карточку с питомцев в центральный айтем
-  ITEM_ACTIVE.appendChild(card1);
-};
-
-// сгенерируем три карточки для активного айтема
-for (let i = 0; i < 3; i += 1) {
-  setInitialCentralItem(uniqueNumbers[i]);
-}
-
-const setLeftItem = (petKey) => {
-  // ITEM_ACTIVE.innerHTML = '';
-
+const setCentralItem = (petKey) => {
   const createCardTemplate = () => {
     const card = document.createElement('div');
     card.classList.add('card');
@@ -211,7 +130,6 @@ const setLeftItem = (petKey) => {
   // внутри дива мы можем создавать разные элементы, например createElementImage, createElementButton, итд, и все это положить внутрь нашего дива при помощи appendChild, нужно не забыть задать новым элементам новые классы.
   const card = createCardTemplate();
 
-  // const petKey = Math.floor(Math.random() * 7);
   // создадим элемент img
   const img = document.createElement('img');
   img.classList.add('card__image');
@@ -223,7 +141,6 @@ const setLeftItem = (petKey) => {
   // добавим в карточку имя питомца
   const petName = document.createElement('p');
   petName.classList.add('card__pet-name');
-  // console.log(petName);
   petName.innerText = petsData[petKey].name;
   card.appendChild(petName);
 
@@ -236,18 +153,83 @@ const setLeftItem = (petKey) => {
   card.appendChild(buttonLearnMore);
 
   // добавим карточку с питомцев в центральный айтем
+  ITEM_ACTIVE.appendChild(card);
+};
+
+// сгенерируем три карточки для активного айтема
+for (let i = 0; i < 3; i += 1) {
+  setCentralItem(petsNums[i]);
+}
+
+const setLeftItem = (petKey) => {
+  const createCardTemplate = () => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    return card;
+  };
+
+  // создадим карточку питомца
+  const card = createCardTemplate();
+
+  // создадим элемент img
+  const img = document.createElement('img');
+  img.classList.add('card__image');
+  // зададим элементу img src
+  img.src = petsData[petKey].img;
+  // добавим элемент img внутрь нашей карточки
+  card.appendChild(img);
+
+  // добавим в карточку имя питомца
+  const petName = document.createElement('p');
+  petName.classList.add('card__pet-name');
+  petName.innerText = petsData[petKey].name;
+  card.appendChild(petName);
+
+  // добавим в карточку кнопку learn more
+  const buttonLearnMore = document.createElement('button');
+  buttonLearnMore.type = 'button';
+  buttonLearnMore.classList.add('button-secondary');
+  buttonLearnMore.classList.add('card__button');
+  buttonLearnMore.innerText = 'Learn more';
+  card.appendChild(buttonLearnMore);
+
   ITEM_LEFT.appendChild(card);
 };
 
-// сгенерируем три карточки для левого айтема
-// for (let i = 0; i < 3; i += 1) {
-//   setLeftItem(uniqueNumbers[i]);
-// }
+const setRightItem = (petKey) => {
+  const createCardTemplate = () => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    return card;
+  };
 
-// По идее и для правого айтема тоже будут те же карточки сгенерированы. Просто мы в зависимости, от того, влево или вправо пользователь кликнет, и передадим эти карточки, или в левый или в правый айтем
-// for (let i = 0; i < 3; i += 1) {
-//   setLeftItem(uniqueNumbers[i]);
-// }
+  // создадим карточку питомца
+  const card = createCardTemplate();
+
+  // создадим элемент img
+  const img = document.createElement('img');
+  img.classList.add('card__image');
+  // зададим элементу img src
+  img.src = petsData[petKey].img;
+  // добавим элемент img внутрь нашей карточки
+  card.appendChild(img);
+
+  // добавим в карточку имя питомца
+  const petName = document.createElement('p');
+  petName.classList.add('card__pet-name');
+  petName.innerText = petsData[petKey].name;
+  card.appendChild(petName);
+
+  // добавим в карточку кнопку learn more
+  const buttonLearnMore = document.createElement('button');
+  buttonLearnMore.type = 'button';
+  buttonLearnMore.classList.add('button-secondary');
+  buttonLearnMore.classList.add('card__button');
+  buttonLearnMore.innerText = 'Learn more';
+  card.appendChild(buttonLearnMore);
+
+  ITEM_RIGHT.appendChild(card);
+};
 
 CAROUSEL.addEventListener('animationend', (animationEvent) => {
   if (animationEvent.animationName === 'move-left') {
@@ -255,13 +237,11 @@ CAROUSEL.addEventListener('animationend', (animationEvent) => {
 
     const leftItems = document.querySelector('#item-left').innerHTML;
     ITEM_ACTIVE.innerHTML = leftItems;
-
-    // сгенерируем три карточки для левого айтема
-    // for (let i = 3; i < 6; i += 1) {
-    //   setLeftItem(uniqueNumbers[i]);
-    // }
   } else {
     CAROUSEL.classList.remove('transition-right');
+
+    const rightItems = document.querySelector('#item-right').innerHTML;
+    ITEM_ACTIVE.innerHTML = rightItems;
   }
 
   BTN_LEFT.addEventListener('click', moveLeft);
